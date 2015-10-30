df47f4f44366edf57bf6f07cf25cbcf603957015
ShawkiShehadeh <shawkishehadeh77@gmail.com>
2015-10-30 18:37:15 +0200

    First commit

---
+++ build/generated-sources/ap-source-output/jpa/entities/Centers_.java
@@ -0,0 +1,14 @@
+package jpa.entities;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-10-30T15:33:06")
+@StaticMetamodel(Centers.class)
+public class Centers_ { 
+
+    public static volatile SingularAttribute<Centers, Integer> cntrNum;
+    public static volatile SingularAttribute<Centers, String> cntrName;
+
+}
---
+++ build/generated-sources/ap-source-output/jpa/entities/UserDetails_.java
@@ -0,0 +1,15 @@
+package jpa.entities;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-10-30T15:33:06")
+@StaticMetamodel(UserDetails.class)
+public class UserDetails_ { 
+
+    public static volatile SingularAttribute<UserDetails, String> password;
+    public static volatile SingularAttribute<UserDetails, String> userName;
+    public static volatile SingularAttribute<UserDetails, Short> userId;
+
+}
